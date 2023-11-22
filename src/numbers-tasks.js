e1755659399585abb37effe5c275886bca279bf2
Maria <workmail358@gmail.com>
2023-11-22 22:07:31 +0300

    feat: solve task isNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -413,8 +413,11 @@ function getNumberValue(number) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  if (Number.isFinite(number)) {
+    return true;
+  }
+  return false;
 }
 
 /**
